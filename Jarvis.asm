1111aa05e87075aa1c6f7c758e0ff58d30f589c0
Mathias Collas-Jourdan <mathias.collas@gmail.com>
2024-02-03 01:27:57 +0100

    Update Jarvis.asm

--- Jarvis.asm
+++ Jarvis.asm
@@ -19,8 +19,6 @@ extern printf
 extern exit
 extern scanf
 extern rand
-extern malloc
-extern free
 extern exit
 
 %define	StructureNotifyMask	131072
@@ -49,9 +47,9 @@ height:        	resd	1
 window:		resq	1
 gc:		resq	1
 
-nbPoint:        resd 1
-xCoordinates: resd 50  ; Tableau pour les coordonnées x
-yCoordinates: resd 50  ; Tableau pour les coordonnées y
+
+xCoordinates resd 25  ; Réserve de l'espace pour un pointeur
+yCoordinates resd 25  ; Réserve de l'espace pour un pointeur
 
 section .data
 
@@ -62,27 +60,13 @@ x2:	dd	0
 y1:	dd	0
 y2:	dd	0
 
-question:   db "entrez un nombre : ",0
 fmt_print: db "Valeur : %d",10,0
 fmt_scanf_int: db "%d",0
-message_error_big: db "Error, too big",10,0
-message_error_small: db "Error, too small",10,0
+nbPoint equ 25
 random_min equ 100  ; constante borne min pour les coordonnées des points
 random_max equ 300  ; constante borne max pour les coordonnées des points
 
 section .text
-
-
-global createDynamicArray
-createDynamicArray:
-    ; Paramètre : rdi = nombre d'éléments
-    ; Retour : rax = adresse du tableau alloué
-    mov rdx, rdi
-    imul rdi, 4  ; Chaque élément est un entier (4 octets)
-    push rdi
-    call malloc
-    add rsp, 8
-    ret
 
 global generateRandomNumber
 generateRandomNumber:
@@ -93,6 +77,7 @@ generateRandomNumber:
     xor rdx, rdx          ; Nettoie rdx avant la division
     div rdi               ; eax / rdi, quotient dans eax, reste dans edx
     add edx, random_min   ; Ajoute random_min au reste pour obtenir un nombre dans la plage [random_min, random_max]
+    
     mov rax, rdx          ; Déplace le résultat final dans rax pour le retour
     ret
 
@@ -103,90 +88,29 @@ generateRandomNumber:
 main:
 
 push rbp
-boucle_select_nb_point:
-    mov rdi, question
-    mov rax, 0
-    call printf
-    mov rdi, fmt_scanf_int
-    mov rsi, nbPoint
-    mov rax, 0
-    call scanf
 
-    cmp dword [nbPoint], 10
-    jb error_small
-    cmp dword [nbPoint], 50
-    ja error_big
-    jmp tableauCoordonnees
-
-error_small:
-    mov rdi, message_error_small
-    mov rax, 0
-    call printf
-    jmp boucle_select_nb_point
-
-error_big:
-    mov rdi, message_error_big
-    mov rax, 0
-    call printf
-    jmp boucle_select_nb_point
-
-    
 tableauCoordonnees:
-    ; Appel de createDynamicArray pour allouer le tableau xCoordinates
-    mov rdi, [nbPoint]
-    call createDynamicArray
-    ; Stockage de l'adresse du tableau dans xCoordinates
-    mov qword [xCoordinates], rax
-
-    ; Appel de createDynamicArray pour allouer le tableau yCoordinates
-    mov rdi, [nbPoint]
-    call createDynamicArray
-    ; Stockage de l'adresse du tableau dans yCoordinates
-    mov qword [yCoordinates], rax
-    
-;############################ Début du remplissage du tableau xCoordinates
-
 ; Initialisation de l'index pour la boucle
-xor rcx, rcx             ; rcx sera notre compteur/index
+xor ebx, ebx             ; rcx sera notre compteur/index
 
-fill_xCoordinates:
-    cmp rcx, [nbPoint]      ; Compare l'index avec nbPoint
-    jge end_fill_x          ; Si rcx >= nbPoint, la boucle est terminée
+fill_Coordinates:
+    cmp ebx, nbPoint  ; Vérifie si on a rempli 50 éléments
+    jge createWindow
 
-    ; Génération d'un nombre aléatoire pour xCoordinates
+    ; Génération d'une valeur aléatoire dans eax
     call generateRandomNumber
+    ; Stocke la valeur générée dans le tableau
+    mov [xCoordinates + ebx*DWORD], eax
     
-    mov rdi, fmt_print
-    mov rsi, 123456789   ; Valeur de test
-    xor rax, rax
-    call printf
     
-    mov rdi, [xCoordinates] ; Récupère le pointeur vers xCoordinates
-    mov [rdi + rcx*4], eax  ; Stocke la valeur aléatoire à la position actuelle
-
-    inc rcx                 ; Incrémente l'index
-    jmp fill_xCoordinates   ; Boucle
-
-end_fill_x:
-
-xor rcx, rcx             ; Réinitialise l'index pour la boucle
-
-fill_yCoordinates:
-    cmp rcx, [nbPoint]      ; Compare l'index avec nbPoint
-    jge end_fill_y          ; Si rcx >= nbPoint, la boucle est terminée
-
-    ; Génération d'un nombre aléatoire pour yCoordinates
     call generateRandomNumber
-    mov rdi, [yCoordinates] ; Récupère le pointeur vers yCoordinates
-    mov [rdi + rcx*4], eax  ; Stocke la valeur aléatoire à la position actuelle
-
-    inc rcx                 ; Incrémente l'index
-    jmp fill_yCoordinates   ; Boucle
-
-end_fill_y:
+    ; Stocke la valeur générée dans le tableau Y
+    mov [yCoordinates + ebx*DWORD], eax
     
+    inc ebx  ; Incrémente l'index pour le prochain élément
+    jmp fill_Coordinates
     
-
+createWindow:
         
 xor     rdi,rdi
 call    XOpenDisplay	; Création de display
@@ -251,20 +175,26 @@ jmp boucle
 ;#		DEBUT DE LA ZONE DE DESSIN		 #
 ;#########################################
 dessin:
+xor ebx, ebx ;compteur de points
+
+drawPoint:
+
+cmp ebx, nbPoint
+jge end_draw
 
 ;couleur du point 1
 mov rdi,qword[display_name]
 mov rsi,qword[gc]
-mov edx,0xFF0000	; Couleur du crayon ; rouge
+mov edx,0x000FFF	; Couleur du crayon ; bleu
 call XSetForeground
 
-; Dessin d'un point rouge sous forme d'un petit rond : coordonnées (100,200)
+; Dessin d'un point sous forme d'un petit rond : coordonnées
 mov rdi,qword[display_name]
 mov rsi,qword[window]
 mov rdx,qword[gc]
-mov rcx,100		; coordonnée en x du point
+mov rcx,[xCoordinates + ebx*DWORD]		; coordonnée en x du point
 sub ecx,3
-mov r8,200 		; coordonnée en y du point
+mov r8,[yCoordinates + ebx*DWORD] 		; coordonnée en y du point
 sub r8,3
 mov r9,6
 mov rax,23040
@@ -272,6 +202,10 @@ push rax
 push 0
 push r9
 call XFillArc
+
+inc ebx
+jmp drawPoint
+end_draw:
 
 ;couleur de la ligne 1
 mov rdi,qword[display_name]
@@ -310,11 +244,5 @@ closeDisplay:
     mov     rax,qword[display_name]
     mov     rdi,rax
     call    XCloseDisplay
-    
-    mov rdi, qword [xCoordinates]
-    call free
-    mov rdi, qword [yCoordinates]
-    call free
-    
     xor	    rdi,rdi
     call    exit
